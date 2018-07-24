a0931f58213926c09c344b8f58c780728202c4cf
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-24 14:00:06 +0300

    get objects or 404 method

--- AdvancedBlogAPI/posts/views.py
+++ AdvancedBlogAPI/posts/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def post_create(request):
     pass
 
 def post_detail(request):
-    pass
+    instance = get_object_or_404(Post, id=1)
+    context = {
+        'obj': instance
+    }
+    return render(request, 'post_detail.html', context)
 
 def post_list(request):
-    return render(request, 'index.html')
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, 'index.html', context)
 
 def post_update(request):
     pass
