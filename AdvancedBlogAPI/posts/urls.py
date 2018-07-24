d91674f4fb4200029d0699b9b2814c0cbfad0fb3
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-24 14:41:58 +0300

    Dynamic Url Routing

--- AdvancedBlogAPI/posts/urls.py
+++ AdvancedBlogAPI/posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='list'),
     url(r'^create/$', views.post_create, name='create'),
-    url(r'^detail/$', views.post_detail, name='detail'),
+    url(r'^detail/(?P<id>\d+)/$', views.post_detail, name='detail'),
     url(r'^update/$', views.post_detail, name='update'),
     url(r'^delete/$', views.post_detail, name='delete')
     # url(r'^$', views.post_home),
