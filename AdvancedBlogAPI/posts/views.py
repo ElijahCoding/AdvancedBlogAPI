8575c40a58b473a151cf40f19d4091ada5ab23e7
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-25 18:04:41 +0300

    updating view instance

--- AdvancedBlogAPI/posts/views.py
+++ AdvancedBlogAPI/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
 
@@ -9,6 +9,8 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        # message successs
+        return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         'form': form
     }
@@ -32,10 +34,12 @@ def post_list(request):
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
 
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        # message successs
+        return HttpResponseRedirect(instance.get_absolute_url())
 
     context = {
         "title": instance.title,
