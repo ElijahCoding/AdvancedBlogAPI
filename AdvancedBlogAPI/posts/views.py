25304ff3ff6001dfeb4db7a685e1e0af3e006c31
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-05 16:38:12 +0300

    showing an image

--- AdvancedBlogAPI/posts/views.py
+++ AdvancedBlogAPI/posts/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -60,7 +60,7 @@ def listing(request):
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
 
-    form = PostForm(request.POST or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
