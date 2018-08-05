7b26e60ad8c8f580cb9fddeedee95e3c4aad5f7d
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-05 20:16:39 +0300

    advanced blog built

--- AdvancedBlogAPI/posts/models.py
+++ AdvancedBlogAPI/posts/models.py
@@ -2,9 +2,13 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 # Create your models here.
+
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.id, filename)
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    image = models.FileField(null=True, blank=True)
+    image = models.ImageField(upload_to=upload_location, null=True, blank=True)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
