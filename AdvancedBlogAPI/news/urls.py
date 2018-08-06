57f4d8b70da93f81f6949fc85526d666a470e191
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-06 15:13:56 +0300

    news crud

--- AdvancedBlogAPI/news/urls.py
+++ AdvancedBlogAPI/news/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    
+    url(r'^$', views.articles_list, name="articles_list"),
+    url(r'^feeds/', views.feeds_list, name="feeds_list"),
+    url(r'^feeds/create/$', views.new_feed, name="new_feed"),
 ]
--- AdvancedBlogAPI/news/views.py
+++ AdvancedBlogAPI/news/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Article, Feed
+from .forms import FeedForm
 
 # Create your views here.
+def articles_list(request):
+    articles = Article.objects.all()
+    return render(request, 'articles_list.html', { 'articles': articles })
+
+def feeds_list(request):
+    feeds = Feed.objects.all()
+    return render(request, 'feeds_list.html', { 'feeds': feeds })
+
+def new_feed(request):
+    form = FeedForm
+    return render(request, 'new_feed.html', { 'form': form })
