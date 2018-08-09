992feb4a2c82d14f7b3e1c0ddaa5abe978e0139b
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-09 15:15:35 +0300

    user auth app set up

--- AdvancedBlogAPI/Todo/forms.py
+++ AdvancedBlogAPI/Todo/forms.py
@@ -1,7 +1,18 @@
 from django import forms
+from .models import Todo
 
 class TodoForm(forms.Form):
     text = forms.CharField(max_length=120,
                            widget=forms.TextInput(
                                attrs={'class':'form-control', 'placeholder':'Enter Todo'}
                            ))
+
+class NewTodoForm(forms.Form):
+    class Meta:
+        model = Todo
+        fields = ['text']
+        widgets = {
+            'text': forms.TextInput(
+                attrs={'class':'form-control', 'placeholder':'Enter Todo'}
+            )
+        }
--- AdvancedBlogAPI/Todo/views.py
+++ AdvancedBlogAPI/Todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from .models import Todo
-from .forms import TodoForm
+from .forms import TodoForm, NewTodoForm
 
 # Create your views here.
 def index(request):
@@ -17,7 +17,7 @@ def addTodo(request):
     form = TodoForm(request.POST)
 
     if form.is_valid():
-        new_todo = Todo(text=request.POST['text'])
+        new_todo = Todo(text=form.cleaned_data['text']) # request.POST['text']
         new_todo.save()
 
     return redirect('todo:index')
