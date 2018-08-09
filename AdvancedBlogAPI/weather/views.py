f478eb272ed067f175dbcfc20efdc10601d5c8bc
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-09 17:23:30 +0300

    save a city

--- AdvancedBlogAPI/weather/views.py
+++ AdvancedBlogAPI/weather/views.py
@@ -8,7 +8,8 @@ def index(request):
     url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'
 
     if request.method == 'POST':
-        pass
+        form = CityForm(request.POST)
+        form.save()
 
     form = CityForm()
 
