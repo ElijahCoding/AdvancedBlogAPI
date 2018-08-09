be0b67a26ab3d5782135ed6d927cdcfbdc65c873
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-09 16:03:52 +0300

    weather views

--- AdvancedBlogAPI/weather/views.py
+++ AdvancedBlogAPI/weather/views.py
@@ -1,5 +1,20 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'weather/weather.html')
+    url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'
+    city = 'Las Vegas'
+
+    res = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city' : city,
+        'temperature': res['main']['temp'],
+        'description': res['weather'][0]['description'],
+        'icon': res['weather'][0]['icon']
+    }
+
+    context = { 'city_weather': city_weather }
+
+    return render(request, 'weather/weather.html', context)
