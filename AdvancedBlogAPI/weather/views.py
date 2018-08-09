9a06ea6b78aaf547f14a6a91ae86ba89af1beab0
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-09 17:08:55 +0300

    showing cities

--- AdvancedBlogAPI/weather/views.py
+++ AdvancedBlogAPI/weather/views.py
@@ -1,20 +1,28 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 # Create your views here.
 def index(request):
     url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'
     city = 'Las Vegas'
 
-    res = requests.get(url.format(city)).json()
+    weather_data = []
+    
+    cities = City.objects.all()
 
-    city_weather = {
-        'city' : city,
-        'temperature': res['main']['temp'],
-        'description': res['weather'][0]['description'],
-        'icon': res['weather'][0]['icon']
-    }
+    for city in cities:
+        res = requests.get(url.format(city)).json()
 
-    context = { 'city_weather': city_weather }
+        city_weather = {
+            'city' : city.name,
+            'temperature': res['main']['temp'],
+            'description': res['weather'][0]['description'],
+            'icon': res['weather'][0]['icon']
+        }
+
+        weather_data.append(city_weather)
+
+    context = { 'weather_data': weather_data }
 
     return render(request, 'weather/weather.html', context)
